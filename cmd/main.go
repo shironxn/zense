4fa34c82af5eafc1d7454696531ef1d7af30031f
shironxn <qshiro69@gmail.com>
2024-10-11 14:50:53 +0700

    chore: update error handling on config

--- cmd/main.go
+++ cmd/main.go
@@ -19,8 +19,8 @@ import (
 func main() {
 	cfg, err := config.New()
 	if err != nil {
-	logrus.Error(err.Error())
-  }
+		// logrus.Panic(err.Error())
+	}
 
 	db, err := config.NewDatabase(cfg.Database).Connection()
 	if err != nil {
